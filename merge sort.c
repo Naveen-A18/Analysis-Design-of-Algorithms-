6747d6f5827cca7ac2864e2102cfdc9f14206b40
Naveen A <2006naveen18@gmail.com>
2026-03-12 19:17:10 +0530

    Update merge sort.c

--- merge sort.c
+++ merge sort.c
@@ -45,3 +45,36 @@ void simple_merge(int a[],int low,int mid,int high)
 		{
 			c[k]=a[i];
 			i++;
+			k++;
+		}
+		else
+		{
+			c[k]=a[j];
+			j++;
+			k++;
+		}
+	}
+	while(i<=mid)
+		c[k++]=a[i++];
+	while(j<=high)
+		c[k++]=a[j++];
+	for(i=low;i<=high;i++)
+		a[i]=c[i];
+}
+
+
+/*Function to divide the array list */
+void merge_sort(int a[],int low,int high)
+{
+	int mid;
+	if(low<high)
+	{
+		mid=(low+high)/2;
+		{
+			merge_sort(a,low,mid);
+			merge_sort(a,mid+1,high);
+		}
+	simple_merge(a,low,mid,high);
+	}
+}
+
