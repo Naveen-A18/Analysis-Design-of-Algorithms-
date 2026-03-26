488e64c92aabaafa4a21bd98aa30cf3d00a5b15d
Naveen A <2006naveen18@gmail.com>
2026-03-26 12:31:32 +0530

    Update selection sort.c

--- selection sort.c
+++ selection sort.c
@@ -1,1 +1,12 @@
+Design and implement C/C++ Program to sort a given set of n integer elements using Selection Sort method and compute its time complexity.
+  Run the program for varied values of n> 5000 and record the time taken to sort. Plot a graph of the time taken versus n. The elements 
+can be read from a file or can be generated using the random number generator.
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define MAX 300000
+int n;
+int main()
+{
 
