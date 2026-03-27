a586e1765f7d3a3937aa60fee0ef5b331fdeab0b
Naveen A <2006naveen18@gmail.com>
2026-03-27 20:54:20 +0530

    Update Quick sort.c

--- Quick sort.c
+++ Quick sort.c
@@ -1,1 +1,46 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+#define MAX 500000
+int partition(int a[],int low,int high);
+int swap(int *a,int *b)
+{
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+	return temp;
+}
+
+void quicksort(int a[],int low,int high)
+{
+	int s;
+	if(low<high)
+	{
+		s=partition(a,low,high);
+		quicksort(a,low,s-1);
+		quicksort(a,s+1,high);
+	}
+}
+
+
+int partition(int a[],int low,int high)
+{
+	int  i,j,pivot;
+	pivot=a[low];
+	i=low+1;
+	j=high;
+	while(1)
+	{
+		while((i<high)&&(pivot>=a[i]))
+		i++;
+		while(pivot<a[j])
+		j--;
+		if(i<j)
+		{
+			swap(&a[i],&a[j]);
+		}
+		else
+		{
+			swap(&a[low],&a[j]);
 
